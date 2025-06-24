a038f5a302d2459ff022b13f8cfe8f3cf242fe1d
Sanket Gaikwad <sanketlike63@gmail.com>
2025-06-24 12:21:39 +0530

    chage production api

--- frontend/src/environments/environment.production.ts
+++ frontend/src/environments/environment.production.ts
@@ -3,5 +3,5 @@ export const environment = {
     // apiUrl:'http://ec2-16-170-163-211.eu-north-1.compute.amazonaws.com:3002'
     //apiUrl: 'http://ec2-16-16-25-196.eu-north-1.compute.amazonaws.com:3002'
     //apiUrl: 'http://127.0.0.1:3002'
-    apiUrl: 'https://college.bytrait.com'
+    apiUrl: 'https://school.bytrait.com'
 };
